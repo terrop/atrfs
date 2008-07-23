fc7c5938ec5f1c75d1585d22972f825bfef83589
Ari Roponen <ari.roponen@gmail.com>
2008-07-23 19:37:01 +0300

    Support asc-files

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-# Makefile - 21.7.2008 - 21.7.2008 Ari & Tero Roponen
+# Makefile - 21.7.2008 - 23.7.2008 Ari & Tero Roponen
 
 CFLAGS=$(shell pkg-config --cflags fuse glib-2.0) -g
 LIBS=$(shell pkg-config --libs fuse glib-2.0)
 
-oma: main.o
+oma: main.o asc-srt.o
 	$(CC) -o $@ $^ $(CFLAGS) $(LIBS)
 
 .PHONY: clean
---
+++ asc-srt.c
@@ -0,0 +1,101 @@
+/* asc-srt.c - 23.7.2008 - 23.7.2008 Ari & Tero Roponen */
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _GNU_SOURCE
+
+static bool has_many_languages = false;
+static bool found_language = false;
+
+static void print_correct_text (const char *line, char *lang, FILE *out)
+{
+	char *end = strchr (line+1, ']');
+	if (! end)
+	{
+		fprintf (out, "%s", line);
+		return;
+	}
+	has_many_languages = true;
+
+	char *tags = strdup (line + 1);
+	tags[end - line - 1] = '\0';
+	char *s;
+	for (s = strtok (tags, ","); s; s = strtok (NULL, ","))
+	{
+		if (! strcmp (s, lang))
+		{
+			found_language = true;
+			fprintf (out, "%s", end + 1 + 1); /* space */
+			break;
+		}
+	}
+	free (tags);
+}
+
+char *asc_read_subtitles (char *ascfile, char *lang)
+{
+	char *text = NULL;
+	size_t size;
+	FILE *out = (FILE *)open_memstream (&text, &size);
+
+	FILE *fp = fopen (ascfile, "r");
+	if (! fp)
+		return NULL;
+
+	char *line = NULL;
+	size_t len = 0;
+	bool after_empty = true;
+	int pos = 0;
+
+	while (getline (&line, &len, fp) > 0)
+	{
+		switch (line[0])
+		{
+		default:
+			fprintf (out, "%s", line);
+			after_empty = false;
+			break;
+		case '\n':
+			after_empty = true;
+			fprintf (out, "%s", line);
+		case '#':	/* comment */
+			break;
+		case '0' ... '9': /* possible timestamp */
+			if (after_empty)
+				fprintf (out, "%d\n", pos++);
+			fprintf (out, "%s", line);
+			break;
+		case '[':
+			print_correct_text (line, lang, out);
+			break;
+		}
+		free (line);
+		line = NULL;
+	}
+	fclose (fp);
+	fclose (out);
+
+	if (has_many_languages && ! found_language)
+	{
+		free (text);
+		return NULL;
+	}
+	return text;
+}
+
+#if 0
+int main (int argc, char *argv[])
+{
+	if (argc != 3)
+		return 1;
+
+	char *srt = asc_read_subtitles (argv[1], argv[2]);
+	if (srt)
+	{
+		printf ("%s", srt);
+		free (srt);
+	}
+	return 0;
+}
+#endif
