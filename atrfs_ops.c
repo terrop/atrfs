ce731966d6d7d0c496f4674ee578cc9c8667dd44
Tero Roponen <tero.roponen@gmail.com>
2008-07-28 12:59:45 +0300

    Move the remaining atrfs-methods to atrfs_ops.c

--- atrfs_ops.c
+++ atrfs_ops.c
@@ -788,3 +788,80 @@ void atrfs_fsync(fuse_req_t req, fuse_ino_t ino, int datasync, struct fuse_file_
 	tmplog("fsync('%s', %d)\n", ent->name, datasync);
 	fuse_reply_err(req, ENOSYS);
 }
+
+void atrfs_getlk(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi, struct flock *lock)
+{
+	/*
+	 * Test for a POSIX file lock
+	 *
+	 * Introduced in version 2.6
+	 *
+	 * Valid replies:
+	 *   fuse_reply_lock
+	 *   fuse_reply_err
+	 *
+	 * @param req request handle
+	 * @param ino the inode number
+	 * @param fi file information
+	 * @param lock the region/type to test
+	 */
+	struct atrfs_entry *ent = ino_to_entry(ino);
+	tmplog("getlk('%s')\n", ent->name);
+	fuse_reply_err(req, ENOSYS);
+}
+
+void atrfs_setlk(fuse_req_t req, fuse_ino_t ino,
+	struct fuse_file_info *fi, struct flock *lock, int sleep)
+{
+	/**
+	 * Acquire, modify or release a POSIX file lock
+	 *
+	 * For POSIX threads (NPTL) there's a 1-1 relation between pid and
+	 * owner, but otherwise this is not always the case.  For checking
+	 * lock ownership, 'fi->owner' must be used.  The l_pid field in
+	 * 'struct flock' should only be used to fill in this field in
+	 * getlk().
+	 *
+	 * Note: if the locking methods are not implemented, the kernel
+	 * will still allow file locking to work locally.  Hence these are
+	 * only interesting for network filesystems and similar.
+	 *
+	 * Introduced in version 2.6
+	 *
+	 * Valid replies:
+	 *   fuse_reply_err
+	 *
+	 * @param req request handle
+	 * @param ino the inode number
+	 * @param fi file information
+	 * @param lock the region/type to test
+	 * @param sleep locking operation may sleep
+	 */
+	struct atrfs_entry *ent = ino_to_entry(ino);
+	tmplog("setlk('%s')\n", ent->name);
+	fuse_reply_err(req, ENOSYS);
+}
+
+void atrfs_bmap(fuse_req_t req, fuse_ino_t ino, size_t blocksize, uint64_t idx)
+{
+	/*
+	 * Map block index within file to block index within device
+	 *
+	 * Note: This makes sense only for block device backed filesystems
+	 * mounted with the 'blkdev' option
+	 *
+	 * Introduced in version 2.6
+	 *
+	 * Valid replies:
+	 *   fuse_reply_bmap
+	 *   fuse_reply_err
+	 *
+	 * @param req request handle
+	 * @param ino the inode number
+	 * @param blocksize unit of block index
+	 * @param idx block index within file
+	 */
+	struct atrfs_entry *ent = ino_to_entry(ino);
+	tmplog("bmap('%s')\n", ent->name);
+	fuse_reply_err(req, ENOSYS);
+}
--- main.c
+++ main.c
@@ -234,84 +234,11 @@ void populate_stat_dir(struct atrfs_entry *statroot)
 	update_stats();
 }
 
-static void atrfs_getlk(fuse_req_t req, fuse_ino_t ino,
-	struct fuse_file_info *fi, struct flock *lock)
-{
-	/*
-	 * Test for a POSIX file lock
-	 *
-	 * Introduced in version 2.6
-	 *
-	 * Valid replies:
-	 *   fuse_reply_lock
-	 *   fuse_reply_err
-	 *
-	 * @param req request handle
-	 * @param ino the inode number
-	 * @param fi file information
-	 * @param lock the region/type to test
-	 */
-	struct atrfs_entry *ent = ino_to_entry(ino);
-	tmplog("getlk('%s')\n", ent->name);
-	fuse_reply_err(req, ENOSYS);
-}
-
-static void atrfs_setlk(fuse_req_t req, fuse_ino_t ino,
-	struct fuse_file_info *fi, struct flock *lock, int sleep)
-{
-	/**
-	 * Acquire, modify or release a POSIX file lock
-	 *
-	 * For POSIX threads (NPTL) there's a 1-1 relation between pid and
-	 * owner, but otherwise this is not always the case.  For checking
-	 * lock ownership, 'fi->owner' must be used.  The l_pid field in
-	 * 'struct flock' should only be used to fill in this field in
-	 * getlk().
-	 *
-	 * Note: if the locking methods are not implemented, the kernel
-	 * will still allow file locking to work locally.  Hence these are
-	 * only interesting for network filesystems and similar.
-	 *
-	 * Introduced in version 2.6
-	 *
-	 * Valid replies:
-	 *   fuse_reply_err
-	 *
-	 * @param req request handle
-	 * @param ino the inode number
-	 * @param fi file information
-	 * @param lock the region/type to test
-	 * @param sleep locking operation may sleep
-	 */
-	struct atrfs_entry *ent = ino_to_entry(ino);
-	tmplog("setlk('%s')\n", ent->name);
-	fuse_reply_err(req, ENOSYS);
-}
-
-static void atrfs_bmap(fuse_req_t req, fuse_ino_t ino, size_t blocksize, uint64_t idx)
-{
-	/*
-	 * Map block index within file to block index within device
-	 *
-	 * Note: This makes sense only for block device backed filesystems
-	 * mounted with the 'blkdev' option
-	 *
-	 * Introduced in version 2.6
-	 *
-	 * Valid replies:
-	 *   fuse_reply_bmap
-	 *   fuse_reply_err
-	 *
-	 * @param req request handle
-	 * @param ino the inode number
-	 * @param blocksize unit of block index
-	 * @param idx block index within file
-	 */
-	struct atrfs_entry *ent = ino_to_entry(ino);
-	tmplog("bmap('%s')\n", ent->name);
-	fuse_reply_err(req, ENOSYS);
-}
-
+extern void atrfs_getlk(fuse_req_t req, fuse_ino_t ino,
+	struct fuse_file_info *fi, struct flock *lock);
+extern void atrfs_setlk(fuse_req_t req, fuse_ino_t ino,
+	struct fuse_file_info *fi, struct flock *lock, int sleep);
+extern void atrfs_bmap(fuse_req_t req, fuse_ino_t ino, size_t blocksize, uint64_t idx);
 extern void atrfs_init(void *userdata, struct fuse_conn_info *conn);
 extern void atrfs_destroy(void *userdata);
 extern void atrfs_lookup(fuse_req_t req, fuse_ino_t parent, const char *name);
