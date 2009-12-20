224a23f2de41477055622f889341fa4adb14bf22
Tero Roponen <tero.roponen@gmail.com>
2009-12-20 11:21:21 +0200

    Remove unneeded wrapper

--- atrfs_init.c
+++ atrfs_init.c
@@ -14,45 +14,25 @@
 /* in main.c */
 extern struct atrfs_entry *statroot;
 
-static char *get_group_name(char *fname)
-{
-	static char gname[256];
-#if 0
-	int idx;
-	if (sscanf(fname, "%[^_]_%2d.flv", gname, &idx) == 2)
-	{
-		strcat(gname, ".flv");
-		return gname;
-	}
-	return fname;
-#else
-	char *uniq_name = uniquify_name(fname, root);
-	strcpy(gname, uniq_name);
-	free(uniq_name);
-	return gname;
-#endif
-}
-
 static void add_file_when_flv(const char *filename)
 {
-	char *groupname;
+	char *uniq_name;
 	char *ext = strrchr (filename, '.');
 	if (!ext || strcmp (ext, ".flv")) /* Add only flv-files. */
 		return;
 
-	tmplog ("%s\n", filename);
-	groupname = get_group_name (basename (filename));
-	tmplog ("group: '%s'\n", groupname);
+	uniq_name = uniquify_name(basename(filename), root);
 
-	struct atrfs_entry *ent = lookup_entry_by_name (root, groupname);
+	struct atrfs_entry *ent = lookup_entry_by_name (root, uniq_name);
 
 	if (!ent)
 	{
 		ent = create_entry (ATRFS_FILE_ENTRY);
-		attach_entry (root, ent, groupname);
+		attach_entry (root, ent, uniq_name);
 	}
 
 	ent->file.real_path = strdup(filename);
+	free(uniq_name);
 }
 
 extern char *language_list;
