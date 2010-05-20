574b1abd0c8a975a032fae7f98ff260dc1d6740c
Ari Roponen <ari.roponen@gmail.com>
2010-05-20 09:02:54 +0300

    Remove unused code

--- atrfs_init.c
+++ atrfs_init.c
@@ -22,32 +22,6 @@
  */
 void atrfs_init(void *userdata, struct fuse_conn_info *conn)
 {
-#if 0
-	int categorize_flv_helper (struct atrfs_entry *ent)
-	{
-		if (ent->e_type == ATRFS_FILE_ENTRY)
-			categorize_flv_entry (ent);
-		return 0;
-	}
-
-	char *pwd = get_current_dir_name();
-	tmplog("init(pwd='%s')\n", pwd);
-	free(pwd);
-
-	language_list = strdup("fi, it, en, la\n");
-	root = create_entry (ATRFS_DIRECTORY_ENTRY);
-	root->name = "/";
-
-	statroot = create_entry (ATRFS_DIRECTORY_ENTRY);
-	attach_entry (root, statroot, "stats");
-
-	parse_config_file ((char *)userdata, root);
-	populate_stat_dir (statroot);
-
-	/* Put file into right category directory. We assume that
-	 * there are only flv-files. */
-	map_leaf_entries (root, categorize_flv_helper);
-#endif
 }
 
 /*
